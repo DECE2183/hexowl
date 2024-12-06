4a85e2a102ef04063fac8f80e320abfa81e8cd04
DECE2183 <tim.korolik@gmail.com>
2024-12-06 18:25:59 +0300

    Change version.

--- builtin/constants.go
+++ builtin/constants.go
@@ -15,7 +15,7 @@ var constants = types.ConstantMap{
 	"true":    true,
 	"false":   false,
 	"help":    "Type in the expression you want to calc and press Enter to get the result.\n\tTo define a variable type its name and assign the value with '=' operator.\n\tType 'funcs()' to see all available functions.\n\tType 'vars()' to see all available variables.",
-	"version": "1.4.2",
+	"version": "1.5.0",
 }
 
 // Is constant with name presented in the builtin constant map.
